7cad0e274e7294a4f60dfe3fa46c6c78cb68286d
ValentinaLuchinovich <valinkontakt11@bk.ru>
2024-06-27 12:47:33 +0300

    reddot

--- CustomUIKitStatick/CustomUIKitStatick.swift
+++ CustomUIKitStatick/CustomUIKitStatick.swift
@@ -31,3 +31,27 @@ public class VioletDot: UIView {
     
 }
 
+
+/// Фиолетовый круг
+public class RedDot: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        snp.makeConstraints { make in
+            make.size.equalTo(20)
+        }
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height / 2
+    }
+    
+    
+}
+
